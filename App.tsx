2b437de6c7daa796204bf2f33cadc3f1b6f07217
Malcolm453 <malcolm@vetro.co.za>
2024-01-08 00:36:34 +0200

    added: full stepzen configuration, modal and customer screen

--- App.tsx
+++ App.tsx
@@ -1,27 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
-import { TailwindProvider } from 'tailwind-rn';
-import utilities from './tailwind.json';
-import { NavigationContainer } from '@react-navigation/native';
-import RootNavigator from './navigator/RootNavigator';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { TailwindProvider } from "tailwind-rn";
+import utilities from "./tailwind.json";
+import { NavigationContainer } from "@react-navigation/native";
+import RootNavigator from "./navigator/RootNavigator";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { STEPZEN_API_URL, STEPZEN_API_KEY } from "@env";
+
+const httpLink = createHttpLink({
+  uri: STEPZEN_API_URL,
+  headers: {
+    Authorization: `Apikey ${STEPZEN_API_KEY}`,
+    "Content-Type": "application/json",
+  },
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
     // @ts-ignore - TailwindProvider is missing a type definition
     <TailwindProvider utilities={utilities}>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <RootNavigator />
+        </NavigationContainer>
+      </ApolloProvider>
     </TailwindProvider>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: ["STEPZEN_API_URL", "STEPZEN_API_KEY"],
+          blacklist: null,
+          whitelist: null,
+          safe: true,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
+    ],
   };
 };
